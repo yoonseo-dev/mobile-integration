2fffe36616d55fd876c955272c8cf283d42afaf3
yoonseo_dev <yoonseo0127@gmail.com>
2026-04-02 17:26:12 +0900

    아이콘까지

--- example/lib/ui/pages/todo/bottom_navigation/widget/nav_button.dart
+++ example/lib/ui/pages/todo/bottom_navigation/widget/nav_button.dart
@@ -15,9 +15,15 @@ class NavButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 60,
       decoration: BoxDecoration(
         color: selected ? Process.done.primaryColor : Colors.white,
         borderRadius: .circular(50),
+      ),
+      child: Column(
+        children: [
+          Icon(Icons.home, color: selected ? Colors.white : Colors.grey),
+        ],
       ),
     );
   }
